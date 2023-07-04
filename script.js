f852669bc890060c705390a30c3d4150903c1902
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-07-04 12:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-function calculateVolume() {
-  // Get the radius value from the input field
-  const radius = document.getElementById('radius').value;
+// function calculateVolume() {
+//   // Get the radius value from the input field
+//   const radius = document.getElementById('radius').value;
+
+//   // Calculate the volume of the sphere
+//   const volume = volume_sphere(radius);
+
+//   // Display the volume in the output field
+//   document.getElementById('volume').textContent = volume;
+// }
+
+// function volume_sphere(radius) {
+//   // Calculate the volume of the sphere using the formula (4/3) * π * r^3
+//   const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+//   // Round the volume to 2 decimal places
+//   return volume.toFixed(2);
+// }
+
+
+//
+
+function volume_sphere() {
+  // Get the value of the radius input field
+  const radius = document.getElementById("radius").value;
 
   // Calculate the volume of the sphere
-  const volume = volume_sphere(radius);
-
-  // Display the volume in the output field
-  document.getElementById('volume').textContent = volume;
-}
-
-function volume_sphere(radius) {
-  // Calculate the volume of the sphere using the formula (4/3) * π * r^3
   const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
-  // Round the volume to 2 decimal places
-  return volume.toFixed(2);
+  // Display the volume in the volume field
+  document.getElementById("volume").value = volume.toFixed(2);
 }
